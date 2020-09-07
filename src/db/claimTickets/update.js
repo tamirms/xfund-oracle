8ac49bdd9508de9d3cb97f1597b916e07ac59536
Codegnosis <paul@codegnosis.co.uk>
2020-09-07 17:05:31 +0100

    wrap claim ticket inser in a db transaction & check existence of mainchainTx. Integrate emissions updates into process

--- src/db/claimTickets/update.js
+++ src/db/claimTickets/update.js
@@ -1,17 +1,68 @@
-const { ClaimTickets } = require("../database")
+const { Op } = require("sequelize")
+const { database, ClaimTickets, Emissions } = require("../database")
 const { TICKET_CLAIM_STATUS } = require("../../common/utils/constants")
 
-const insertClaimTicket = async (amount, ethAddress, nonce, ticket, mainchainTx, validatorId) => {
-  const insertRes = await ClaimTickets.create({
-    amount,
-    ethAddress,
-    nonce,
-    ticket,
-    mainchainTx,
-    validatorId,
-    claimStatus: TICKET_CLAIM_STATUS.ISSUED,
-  })
-  return insertRes.id
+const insertClaimTicket = async (amount, ethAddress, nonce, mainchainTx, validatorId, emissionIds) => {
+  try {
+    const result = await database.transaction(async (t) => {
+      let insertRes
+      const byTxRes = await ClaimTickets.findOne({
+        where: {
+          mainchainTx,
+        },
+        transaction: t,
+      })
+
+      if (!byTxRes) {
+        insertRes = await ClaimTickets.create(
+          {
+            amount,
+            ethAddress,
+            nonce,
+            mainchainTx,
+            validatorId,
+            claimStatus: TICKET_CLAIM_STATUS.INITIALISED,
+          },
+          { transaction: t },
+        )
+      }
+
+      // todo: also chain emission updates
+      return insertRes
+    })
+
+    if (result) {
+      await Emissions.update(
+        {
+          claimTicketId: result.id,
+        },
+        {
+          where: {
+            id: { [Op.in]: emissionIds },
+          },
+        },
+      )
+      return result.id
+    }
+    return 0
+  } catch (error) {
+    console.log(error.toString())
+    throw error
+  }
+}
+
+const updateClaimTicketWithTicket = async (claimTicketId, ticket) => {
+  await ClaimTickets.update(
+    {
+      ticket,
+      claimStatus: TICKET_CLAIM_STATUS.ISSUED,
+    },
+    {
+      where: {
+        id: claimTicketId,
+      },
+    },
+  )
 }
 
 const updateClaimTicketWithEthTx = async (claimTicketId, ethTx) => {
@@ -31,4 +82,5 @@ const updateClaimTicketWithEthTx = async (claimTicketId, ethTx) => {
 module.exports = {
   insertClaimTicket,
   updateClaimTicketWithEthTx,
+  updateClaimTicketWithTicket,
 }
